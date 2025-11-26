5004dc50933a99c24f46d45193201d701dda729e
Mazhar-Munir <mazharmunir5065@gmail.com>
2025-11-26 22:22:05 +0500

    fatch api
    fatch api

--- About API/aap.js
+++ About API/aap.js
@@ -1,10 +1,28 @@
  const url="https://catfact.ninja/fact";
-
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn"); 
  
- const getFacts = async() =>{
-    console.log("getting data...");
-    let responce = await fetch(url);
- console.log(responce);  //responce in json format
- let data = await responce.json();
- console.log(data);
- }; 
+// Using async await
+
+//  const getFacts = async() =>{
+//     console.log("getting data...");
+//     let responce = await fetch(url);
+//  console.log(responce);  //responce in json format
+//  let data = await responce.json();
+ 
+//  factPara.innertext = data[1 ].text;
+//  }; 
+ 
+// Using Promises chain
+// function getFacts(){
+//    fetch(url)
+//    .then((Response)=>{
+//       return Response.json();
+//    })
+//    .then((data) =>{
+//       console.log(data);
+//       factPara.innertext = data[1].text;
+//    }) ;
+// }
+ 
+btn.addEventListener("click", getFacts);
