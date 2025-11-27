6ea858b3d0cbb8e54ac733715fdf6958eac10709
Mazhar-Munir <mazharmunir5065@gmail.com>
2025-11-27 23:55:52 +0500

    project using api fetch
    project using api fetch

--- About API/Learn about Api/aap.js
+++ About API/Learn about Api/aap.js
@@ -4,14 +4,14 @@ const btn = document.querySelector("#btn");
  
 // Using async await
 
-//  const getFacts = async() =>{
-//     console.log("getting data...");
-//     let responce = await fetch(url);
-//  console.log(responce);  //responce in json format
-//  let data = await responce.json();
+ const getFacts = async() =>{
+    console.log("getting data...");
+    let responce = await fetch(url);
+ console.log(responce);  //responce in json format
+ let data = await responce.json();
  
-//  factPara.innertext = data[1 ].text;
-//  }; 
+ factPara.innertext = data[1 ].text;
+ }; 
  
 // Using Promises chain
 // function getFacts(){
